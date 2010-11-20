4c2b91aa0a318c61571a785978d97baf27cd4509
Alex Wright <alexkwright@gmail.com>
2010-11-20 20:14:05 +0000

    Wrapping functions in a closure.

--- JRPad.js
+++ JRPad.js
@@ -21,106 +21,110 @@
 //stop browser from initiating its drag function
 document.ondragstart = function(e) { return false; }
 
-function JRPad(domElementID)
-{
-	//functions
-	this.setMouse = setMouse;
-	this.drawDot = drawDot;
-	this.drawLine = drawLine;
-	this.eraser = eraser;
-	this.setColor = setColor;
-	this.setWidth = setWidth;
-	
-	//variables
-	this.id = domElementID;
-	this.fullID = "#" + domElementID;
-	
-	this.strokeWidth = 1;
-	this.strokeColor = "#000000";
-	this.backgroundColor = "#ffffff";
-	
-	var width = $(this.fullID).width();
-	var height = $(this.fullID).height();
-	
-	this.offset = $(this.fullID).offset();
-	
-	this.previousX = 0;
-	this.previousY = 0;
+window['JRPad'] = (function () {
+	function JRPad(domElementID)
+	{
+		//functions
+		this.setMouse = setMouse;
+		this.drawDot = drawDot;
+		this.drawLine = drawLine;
+		this.eraser = eraser;
+		this.setColor = setColor;
+		this.setWidth = setWidth;
+		
+		//variables
+		this.id = domElementID;
+		this.fullID = "#" + domElementID;
+		
+		this.strokeWidth = 1;
+		this.strokeColor = "#000000";
+		this.backgroundColor = "#ffffff";
+		
+		var width = $(this.fullID).width();
+		var height = $(this.fullID).height();
+		
+		this.offset = $(this.fullID).offset();
+		
+		this.previousX = 0;
+		this.previousY = 0;
 
-	this.paper = Raphael(document.getElementById(this.id), width, height);
-	this.background = this.paper.rect(this.offset.left - 8, this.offset.top - 8, width, height).attr({fill: "white"});
-	
-	this.mouseIsDown = false;
-	
-}
+		this.paper = Raphael(document.getElementById(this.id), width, height);
+		this.background = this.paper.rect(this.offset.left - 8, this.offset.top - 8, width, height).attr({fill: "white"});
+		
+		this.mouseIsDown = false;
+		
+	}
 
-function setMouse(isDown)
-{
-	this.mouseIsDown = isDown;
-}
+	function setMouse(isDown)
+	{
+		this.mouseIsDown = isDown;
+	}
 
-//draw dot on single mouse click
-function drawDot(e)
-{
-	var x = e.pageX - this.offset.left;
-	var y = e.pageY - this.offset.top;
-	
-	this.paper.circle(x, y, this.strokeWidth / 2).attr(
-			{
-				stroke: this.strokeColor,
-				fill: this.strokeColor
-			});
-}
-
-function drawLine(e)
-{
-	if(this.mouseIsDown)
+	//draw dot on single mouse click
+	function drawDot(e)
 	{
 		var x = e.pageX - this.offset.left;
 		var y = e.pageY - this.offset.top;
 		
-		if((this.previousX) < x || (this.previousY) < y || (this.previousX) > x || (this.previousY) > y)
+		this.paper.circle(x, y, this.strokeWidth / 2).attr(
+				{
+					stroke: this.strokeColor,
+					fill: this.strokeColor
+				});
+	}
+
+	function drawLine(e)
+	{
+		if(this.mouseIsDown)
 		{
-			//get rid of crazy line from 0,0
-			if(this.previousX == 0 || this.previousY == 0)
+			var x = e.pageX - this.offset.left;
+			var y = e.pageY - this.offset.top;
+			
+			if((this.previousX) < x || (this.previousY) < y || (this.previousX) > x || (this.previousY) > y)
+			{
+				//get rid of crazy line from 0,0
+				if(this.previousX == 0 || this.previousY == 0)
+				{
+					this.previousX = x;
+					this.previousY = y;
+				}
+				this.paper.path("M" + this.previousX + " " + this.previousY + "L" + x + " " + y).attr(
+				{
+					"stroke-width": this.strokeWidth + "px",
+					"stroke-linecap": "round",
+					stroke: this.strokeColor
+				});
+				
+				this.previousX = x;
+				this.previousY = y;
+			}
+			else
 			{
 				this.previousX = x;
 				this.previousY = y;
 			}
-			this.paper.path("M" + this.previousX + " " + this.previousY + "L" + x + " " + y).attr(
-			{
-				"stroke-width": this.strokeWidth + "px",
-				"stroke-linecap": "round",
-				stroke: this.strokeColor
-			});
-			
-			this.previousX = x;
-			this.previousY = y;
 		}
 		else
 		{
-			this.previousX = x;
-			this.previousY = y;
+			this.previousX = 0;
+			this.previousY = 0;
 		}
 	}
-	else
+
+	function eraser()
 	{
-		this.previousX = 0;
-		this.previousY = 0;
+		this.strokeColor = this.backgroundColor;
 	}
-}
 
-function eraser()
-{
-	this.strokeColor = this.backgroundColor;
-}
+	function setColor(colorValue)
+	{
+		this.strokeColor = colorValue;
+	}
 
-function setColor(colorValue)
-{
-	this.strokeColor = colorValue;
-}
-
-function setWidth(widthValue)
-{
-	this.strokeWidth = widthValue
-}
+	function setWidth(widthValue)
+	{
+		this.strokeWidth = widthValue
+	}
+	
+	return JRPad;
+})();
